0507609bc84923be0f3c51562e6c066a5c8dfd6d
Mandy Woodbury <85962983+mixtape85@users.noreply.github.com>
2021-09-06 15:48:04 -0400

    add logic for hasCertainKey and hasCertainValue functions

--- some-every.js
+++ some-every.js
@@ -61,7 +61,9 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+    return arr.every(val => key in val);
+}
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
@@ -79,4 +81,6 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+    return arr.every(val => val[key] === searchValue);
+}
